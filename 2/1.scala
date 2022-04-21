bf436bb8f5eed590462c6ec27e6e5ffb26f03910
HeartPattern <heartpattern@kakao.com>
2022-04-21 09:21:29 +0900

    박진영 문제풀이

--- 2/1.scala
+++ 2/1.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 /**
  * n번째 피보나치 수를 돌려주는 재귀 함수를 작성하라.
  * 처음 두 피보나치 수는 0과 1이다. n번째 피보나치 수는 항상 이전 두 수의 합이다.
@@ -5,4 +7,12 @@
  * 반드시 지역 꼬리 재귀 함수를 사용해서 작성할 것.
  */
 
-def fib(n: Int): Int
+  def fib(n: Int): Int = {
+    @tailrec
+    def fibAdd(n: Int, first : Int, second : Int) : Int = {
+      if(n <= 1) first
+      else if(n == 2) second
+      else fibAdd(n-1, second, first + second)
+    }
+    fibAdd(n, 0, 1)
+  }
--- 2/5.scala
+++ 2/5.scala
@@ -1,4 +1,5 @@
 /**
  * 두 함수를 합성하는 고차 함수를 구현하라.
  */
-def compose[A, B, C](f: B => C, g: A => B): A => C
+def compose[A, B, C](f: B => C, g: A => B): A => C =
+  (a: A) => f(g(a))
