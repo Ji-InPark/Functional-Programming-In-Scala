e323df2c72cc341210fe8f33b67ead5cf40f86f4
Jin Park <jin225675@naver.com>
2022-04-22 23:14:07 +0900

    5번 문제 풀이

--- 3/5.scala
+++ 3/5.scala
@@ -6,4 +6,8 @@ sealed trait List[+A]
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
-def dropWhile[A](l: List[A], f: A => Boolean): List[A]
+def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+  l match {
+    case Cons(h, t) if !f(h) => t
+    case Cons(_, t) => dropWhile(t, f)
+  }
