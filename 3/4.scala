975d661101089aeecbd51f52c35b368bc1cbe8c9
Jin Park <jin225675@naver.com>
2022-04-22 23:11:08 +0900

    4번 문제 풀이

--- 3/4.scala
+++ 3/4.scala
@@ -8,4 +8,8 @@ sealed trait List[+A]
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
-def drop[A](l: List[A], n: Int): List[A]
+def drop[A](l: List[A], n: Int): List[A] =
+  l match {
+    case Cons(_, _) if n == 0 => l
+    case Cons(_, t) if n > 0 => t
+  }
