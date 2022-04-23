4a09567653e20ee60c8b4647871fa395e9aebe36
Jin Park <jin225675@naver.com>
2022-04-23 10:07:45 +0900

    11번 문제 풀이

--- 3/11.scala
+++ 3/11.scala
@@ -5,3 +5,13 @@
 sealed trait List[+A]
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+@annotation.tailrec
+def foldLeft[A, B](as: List[A], z: B)(F: (B, A) => B): B =
+  as match {
+    case Nil => z
+    case Cons(h, t) => foldLeft(t, F(z, h))(F)
+  }
+
+def sum(ints: List[Int]): Int = foldLeft(ints, 0)(_ + _)
+
