3734becda485b4e0437868a3c3590a331f58126c
Jin Park <81283634+Ji-InPark@users.noreply.github.com>
2022-04-23 16:37:31 +0900

    15번 문제 풀이

--- 3/15.scala
+++ 3/15.scala
@@ -8,3 +8,15 @@
 sealed trait List[+A]
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+def appendAll[A](as: List[List[A]]): List[A] =
+  foldRight(as, Nil: List[A])(append)
+
+def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = // 스칼라가 형식 추론을 할 수 있게 개별적 인수그룹에 넣어줌
+  as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
+
+def append[A](a1: List[A], a2: List[A]): List[A] =
+  foldRight(a1, a2)((x, y) => Cons(x, y))
