e6822e42c3e92f496377ec20639a88583c241f59
Jin Park <jin225675@naver.com>
2022-04-25 00:38:35 +0900

    28번 문제 풀이

--- 3/28.scala
+++ 3/28.scala
@@ -1,3 +1,13 @@
 /**
  * List에 대한 함수 map과 비슷하게 트리의 각 요소를 주어진 함수로 수정하는 함수 map을 작성하라.
  */
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](Left: Tree[A], right: Tree[A]) extends Tree[A]
+
+def map[A, B](t: Tree[A])(f: A => B): Tree[B] =
+  t match {
+    case Leaf(x) => Leaf(f(x))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
