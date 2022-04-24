db1e4439ab1770e6d6205e89380a221a15c88ba3
Jin Park <jin225675@naver.com>
2022-04-25 00:04:19 +0900

    23번 문제 풀이

--- 3/23.scala
+++ 3/23.scala
@@ -6,3 +6,11 @@
 sealed trait List[+A]
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+def zipWith[A, B, C](as1: List[A], as2: List[B])(f: (A, B) => C): List[C] =
+  (as1, as2) match {
+    case (_, Nil) => Nil
+    case (Nil, _) => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2)(f))
+    //t1 전승 우승!
+  }
