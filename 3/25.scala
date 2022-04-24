6c314f2676684489995a16dcf60c323e147d655c
Jin Park <jin225675@naver.com>
2022-04-25 00:28:32 +0900

    25번 문제 풀이

--- 3/25.scala
+++ 3/25.scala
@@ -1,3 +1,13 @@
 /**
  * 트리의 노드, 즉 잎(leaf)과 가지(branch)의 개수를 세는 함수 size를 작성하라.
  */
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](Left: Tree[A], right: Tree[A]) extends Tree[A]
+
+def size[A](t: Tree[A]): Int =
+  t match {
+    case Leaf(_) => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
