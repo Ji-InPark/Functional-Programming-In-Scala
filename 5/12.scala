1578889dfb8034e46c946d46cbda153532c31c69
Hajin_Kim <kimhajin@yonsei.ac.kr>
2022-04-30 13:13:19 +0900

    Add test to 12.scala

--- 5/12.scala
+++ 5/12.scala
@@ -41,5 +41,86 @@ object Stream {
     def from(n: Int): Stream[Int] =
 
     def fibs(): Stream[Int] =
+}
 
+// Test
+object Main {
+    /* // Expected output:
+     * 1
+     * 1
+     * 1
+     * **********
+     * 7
+     * 7
+     * 7
+     * 7
+     * 7
+     * 7
+     * 7
+     * **********
+     * 0
+     * 1
+     * 1
+     * 2
+     * 3
+     * 5
+     * 8
+     * 13
+     * 21
+     * 34
+     * **********
+     * state: 0
+     * state: 2
+     * begin
+     * state: 1
+     * state: 2
+     * state: 3
+     * state: 4
+     * state: 5
+     * lazy operation executed
+     * 0.0
+     * 10.0
+     * 20.0
+     * 30.0
+     * 40.0
+     * next
+     * state: 3
+     * state: 4
+     * state: 5
+     * 20.0
+     * 30.0
+     * 40.0
+     */
+    def main(args: Array[String]): Unit = {
+        val subones = Stream.ones.take(3).toList
+        subones.map(println(_))
+
+        println("**********")
+
+        val cs = Stream.constant(7)
+        val subcs = cs.take(7).toList
+        subcs.map(println(_))
+
+        println("**********")
+
+        val fs = Stream.fibs()
+        val subfs = fs.take(10).toList
+        subfs.map(println(_))
+
+        println("**********")
+
+        def stateLogic(s: Int): Option[(Double, Int)] = {
+            println("state: " + s)
+            if (s < 5) Some((s * 10.0, s + 1)) else None
+        }
+        val xs = Stream.unfold(0)(stateLogic)
+        val ys = Stream.unfold(2)(stateLogic)
+        println("begin")
+        val subxs = xs.toList
+        println("lazy operation executed")
+        subxs.map(println(_))
+        println("next")
+        val subys = ys.toList
+        subys.map(println(_))
+    }
 }
