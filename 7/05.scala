44e85c9f0d7be2edc3805ad74b2961eb8dd638e0
Junu Park <pkjoho95@gmail.com>
2022-05-10 17:28:30 +0900

    3 -> 2

--- 7/05.scala
+++ 7/05.scala
@@ -3,30 +3,28 @@
  * 7.5
  * 어려움: 그러한 수단인 함수 sequence를 작성하라. 다른 기본수단은 필요하지 않다. run은 호출하지 말아야 한다.
  */
-import java.util.concurrent.*
+import java.util.concurrent._
+import language.implicitConversions
 
-object Par:
-  opaque type Par[A] = ExecutorService => Future[A]
+object Par {
+  type Par[A] = ExecutorService => Future[A]
+  
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 
-  extension [A](pa: Par[A]) def run(s: ExecutorService): Future[A] = pa(s)
-
-  def unit[A](a: A): Par[A] =
-    es => UnitFuture(a) // `unit` is represented as a function that returns a `UnitFuture`, which is a simple implementation of `Future` that just wraps a constant value. It doesn't use the `ExecutorService` at all. It's always done and can't be cancelled. Its `get` method simply returns the value that we gave it.
-
-  private case class UnitFuture[A](get: A) extends Future[A]:
-    def isDone = true
-    def get(timeout: Long, units: TimeUnit) = get
-    def isCancelled = false
-    def cancel(evenIfRunning: Boolean): Boolean = false
-
-  def fork[A](a: => Par[A]): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
-    es => es.submit(new Callable[A] { def call = a(es).get })
-
-  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a) // `unit` is represented as a function that returns a `UnitFuture`, which is a simple implementation of `Future` that just wraps a constant value. It doesn't use the `ExecutorService` at all. It's always done and can't be cancelled. Its `get` method simply returns the value that we gave it.
+  
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true 
+    def get(timeout: Long, units: TimeUnit) = get 
+    def isCancelled = false 
+    def cancel(evenIfRunning: Boolean): Boolean = false 
+  }
 
   def sequence[A](ps: List[Par[A]]): Par[List[A]] = ???
+}
 
-object Main:
+object Main {
   def main(args: Array[String]): Unit = {
     
   }
+}
--- 7/12.scala
+++ 7/12.scala
@@ -9,30 +9,28 @@
  *
  * 원한다면 책을 잠시 덮고, choice와 choiceN, choiceMap의 구현에 사용할 수 있는 좀 더 일반적인 새 조합기를 고안해 보기 바란다.
  */
-import java.util.concurrent.*
+import java.util.concurrent._
+import language.implicitConversions
 
-object Par:
-  opaque type Par[A] = ExecutorService => Future[A]
+object Par {
+  type Par[A] = ExecutorService => Future[A]
+  
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 
-  extension [A](pa: Par[A]) def run(s: ExecutorService): Future[A] = pa(s)
-
-  def unit[A](a: A): Par[A] =
-    es => UnitFuture(a) // `unit` is represented as a function that returns a `UnitFuture`, which is a simple implementation of `Future` that just wraps a constant value. It doesn't use the `ExecutorService` at all. It's always done and can't be cancelled. Its `get` method simply returns the value that we gave it.
-
-  private case class UnitFuture[A](get: A) extends Future[A]:
-    def isDone = true
-    def get(timeout: Long, units: TimeUnit) = get
-    def isCancelled = false
-    def cancel(evenIfRunning: Boolean): Boolean = false
-
-  def fork[A](a: => Par[A]): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
-    es => es.submit(new Callable[A] { def call = a(es).get })
-
-  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a) // `unit` is represented as a function that returns a `UnitFuture`, which is a simple implementation of `Future` that just wraps a constant value. It doesn't use the `ExecutorService` at all. It's always done and can't be cancelled. Its `get` method simply returns the value that we gave it.
+  
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true 
+    def get(timeout: Long, units: TimeUnit) = get 
+    def isCancelled = false 
+    def cancel(evenIfRunning: Boolean): Boolean = false 
+  }
 
   def chocieN[A](n: Par[Int])(choices: List[Par[A]]): Par[A] = ???
+}
 
-object Main:
+object Main {
   def main(args: Array[String]): Unit = {
     
   }
+}
